30d17386a48aea5b0bbbfe2abc576c4784f921a8
Carlos737392 <97768361+Carlos737392@users.noreply.github.com>
2023-05-14 15:22:54 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -511,7 +511,7 @@ class PlayState extends MusicBeatState
 		switch (curStage)
 		{
 			case 'stage': //Week 1
-				var bg:BGSprite = new BGSprite('stageback', 600, 400, 0.9, 0.9);
+				var bg:BGSprite = new BGSprite('stageback', 0, 400, 0.9, 0.9);
 				bg.scale.set(4.0, 4.0);
 				add(bg);
 
@@ -539,10 +539,10 @@ class PlayState extends MusicBeatState
 
 			case 'spooky': //Week 2
 				if(!ClientPrefs.lowQuality) {
-					halloweenBG = new BGSprite('halloween_bg', 200, 200, ['halloweem bg0', 'halloweem bg lightning strike']);
+					halloweenBG = new BGSprite('halloween_bg', 100, 300, ['halloweem bg0', 'halloweem bg lightning strike']);
 					halloweenBG.scale.set(4.0, 4.0);
 				} else {
-					halloweenBG = new BGSprite('halloween_bg_low', 200, 200);
+					halloweenBG = new BGSprite('halloween_bg_low', 100, 300);
 					halloweenBG.scale.set(4.0, 4.0);
 				}
 				add(halloweenBG);
